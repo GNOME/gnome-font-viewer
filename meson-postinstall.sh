a19b43d5258832578bbb264cbbfad2d44d248447
Matthias Clasen <mclasen@redhat.com>
2018-04-24 18:03:46 -0400

    We don't install any icons
    So no need to updatean icon cache.

--- meson-postinstall.sh
+++ meson-postinstall.sh
@@ -4,7 +4,4 @@
 if [ -z "$DESTDIR" ]; then
   echo Updating desktop database...
   update-desktop-database -q ${MESON_INSTALL_PREFIX}/share/applications
-
-  echo Updating icon cache...
-  gtk-update-icon-cache -q -t -f ${MESON_INSTALL_PREFIX}/share/icons/hicolor
 fi
