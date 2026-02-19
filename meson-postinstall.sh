766e8459b73640cfce703b54cc8ad48fa9c93843
Khalid Abu Shawarib <kas@gnome.org>
2026-02-19 06:49:20 +0300

    build: Use meson GNOME module for post install script

--- meson-postinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Package managers set this so we don't need to run
-if [ -z "$DESTDIR" ]; then
-  echo Updating desktop database...
-  update-desktop-database -q ${MESON_INSTALL_PREFIX}/share/applications
-fi
